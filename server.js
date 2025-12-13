d6256dff8025cb122941c28ff13ef8d639270d47
Munther Alshawi <muntheralshawi@MacBook-Air-von-Munther.local>
2025-12-13 12:35:02 +0300

    server.js and database

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+require('./config/database.js');
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// Sessions
+const session = require('express-session');
+const { MongoStore } = require('connect-mongo');
+
+// Middleware
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const isSignedIn = require('./middleware/is-signed-in');
+const isAdmin = require('./middleware/is-admin');
+
+// Controllers
+const authCtrl = require('./controllers/auth');
+const BooksCtrl = require('./controllers/Books');
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+// Locals
+app.use(passUserToView);
